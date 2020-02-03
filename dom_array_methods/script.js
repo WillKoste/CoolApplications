f199f9f2844edff1be0dc14c93e18503872bada4
Will Koste <willkoste@gmail.com>
2020-02-02 20:18:03 -0500

    DOM Array Methods

---
+++ dom_array_methods/script.js
@@ -0,0 +1,95 @@
+const main = document.querySelector('#main');
+const addUserBtn = document.querySelector('#add-user');
+const doubleBtn = document.querySelector('#double');
+const showMillionairesBtn = document.querySelector('#show-millionaires');
+const sortBtn = document.querySelector('#sort');
+const calculateWealthBtn = document.querySelector('#calculate-wealth');
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+async function getRandomUser(){
+  const res = await fetch('https://randomuser.me/api');
+  const data = await res.json();
+
+  const user = data.results[0];
+
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000)
+  }
+
+  addData(newUser);
+}
+
+
+
+function doubleMoney(){
+  data = data.map((user) => {
+    return {...user, money: user.money * 2}
+  });
+
+  updateDOM();
+}
+
+
+
+function sortByRichest(){
+  data.sort((a, b) =>{
+    return b.money - a.money;
+  });
+
+  updateDOM();
+}
+
+
+
+function showMillionaires(){
+  data = data.filter((item) => {
+    return item.money > 1000000
+  });
+
+  updateDOM();
+}
+
+
+
+function calculateWealth(){
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+  const wealthEl = document.createElement('div');
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+  main.appendChild(wealthEl);
+}
+
+
+
+function addData(obj){
+  data.push(obj);
+
+  updateDOM();
+}
+
+function updateDOM(providedData = data){
+  main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+  providedData.forEach(item => {
+    const element = document.createElement('div');
+    element.classList.add('person');
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
+    main.appendChild(element);
+  });
+}
+
+function formatMoney(number){
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+addUserBtn.addEventListener('click', getRandomUser);
+doubleBtn.addEventListener('click', doubleMoney);
+sortBtn.addEventListener('click', sortByRichest);
+showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
