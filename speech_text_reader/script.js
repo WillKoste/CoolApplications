673ab81a15f464a14ff390cfd4a0e7458cc99331
Will Koste <willkoste@gmail.com>
2020-02-09 18:12:22 -0500

    Speech Text Reader

--- speech_text_reader/script.js
+++ speech_text_reader/script.js
@@ -71,5 +71,63 @@ function createBox(item) {
     <p class="info">${text}</p>
   `;
 
+  box.addEventListener('click', () => {
+    setTextMessage(text);
+    speakText();
+
+    box.classList.add('active');
+    setTimeout(() => {
+      box.classList.remove('active');
+    }, 800);
+  });
+  
   main.appendChild(box);
 }
+
+// Init Speech Synthesis Utterance
+const message = new SpeechSynthesisUtterance();
+
+let voices = [];
+
+function getVoices(){
+  voices = speechSynthesis.getVoices();
+
+  voices.forEach(voice => {
+    const option = document.createElement('option');
+    option.value = voice.name;
+    option.innerText = `${voice.name} ${voice.lang}`;
+
+    voicesSelect.appendChild(option);
+  });
+}
+
+function setTextMessage(text){
+  message.text = text;
+}
+
+function speakText(){
+  speechSynthesis.speak(message);
+}
+
+function setVoice(e){
+  message.voice = voices.find(voice => voice.name === e.target.value);
+}
+
+speechSynthesis.addEventListener('voiceschanged', getVoices);
+
+toggleBtn.addEventListener('click', () => {
+  document.querySelector('#text-box').classList.toggle('show');
+});
+
+closeBtn.addEventListener('click', () => {
+  document.querySelector('#text-box').classList.remove('show');
+});
+
+voicesSelect.addEventListener('change', setVoice);
+
+readBtn.addEventListener('click', () => {
+  setTextMessage(textarea.value);
+  speakText();
+});
+
+getVoices();
